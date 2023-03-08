f1b561546d3a1e988d1e8dd7e6e68a7f5951cd28
MateusDuraessantos <mateusduraessantos@gmail.com>
2023-03-08 11:12:38 -0300

    finalização tela de calendários

--- src/views/Componentes/CalendarioDs.vue
+++ src/views/Componentes/CalendarioDs.vue
@@ -19,13 +19,24 @@
                 período de visualização de informações.
               </p>
               <br />
-              <el-date-picker class="calendario" v-model="value2" type="date" placeholder="Pick a day"
-                :prefix-icon="customPrefix" :size="size" />
+              <el-row>
+                <el-col :span="12">
+                  <h6>Default</h6>
+                  <br>
+                  <el-date-picker class="calendario" v-model="value2" type="date" placeholder="Pick a day"
+                    :prefix-icon="customPrefix" :size="size" />
+                </el-col>
+                <el-col :span="12">
+                  <h6>Disabled</h6>
+                  <br>
+                  <el-date-picker class="calendario" v-model="value2" type="date" placeholder="Pick a day"
+                    :prefix-icon="customPrefix" :size="size" disabled />
+                </el-col>
+              </el-row>
               <!--  -->
 
               <br>
               <br>
-
               <el-collapse v-model="activeNames" @change="handleChange">
                 <el-collapse-item title="Ver código">
                   <div class="html_codigos">
@@ -34,16 +45,31 @@
                       <div recuo>
                         &#60;<span tagColor>el-date-picker</span>
                         <div class="border-left">
-                          <div recuo><span attributeColor>class</span>=<span roxo>"calendario"</span> <br><span
-                              attributeColor>v-model</span>=<span roxo>"value2"</span><br>
-                            <span attributeColor>type</span>=<span roxo>"date"</span> <br> <span
-                              attributeColor>placeholder</span>=<span roxo>"Pick
-                              a day"</span><br>
-                            <span attributeColor>:prefix-icon</span>=<span roxo>"customPrefix"</span><br> <span
-                              attributeColor>:size</span>=<span roxo>"size"</span>
+                          <div recuo>class=<span roxo>"calendario"</span> <br>
+                            v-model=<span roxo>"value2"</span><br>
+                            type=<span roxo>"date"</span> <br>
+                            placeholder=<span roxo>"Pick a day"</span><br>
+                            :prefix-icon=<span roxo>"customPrefix"</span><br>
+                            :size=<span roxo>"size"</span>
                           </div>
                         </div>
-                        <span tagColor></span>/&#62;
+                        /&#62;
+                      </div>
+                    </div>
+                    <div class="border-left">
+                      <div recuo>
+                        &#60;<span tagColor>el-date-picker</span>
+                        <div class="border-left">
+                          <div recuo>class=<span roxo>"calendario"</span> <br>
+                            v-model=<span roxo>"value2"</span><br>
+                            type=<span roxo>"date"</span> <br>
+                            placeholder=<span roxo>"Pick a day"</span><br>
+                            :prefix-icon=<span roxo>"customPrefix"</span><br>
+                            :size=<span roxo>"size"</span><br>
+                            disabled
+                          </div>
+                        </div>
+                        /&#62;
                       </div>
                     </div>
                     &#60;/<span tagColor>template</span>&#62;
@@ -87,8 +113,6 @@
                     <br>
                     &#60;/<span tagColor>script</span>&#62;
                   </div>
-
-
                 </el-collapse-item>
               </el-collapse>
             </div>
@@ -97,23 +121,31 @@
             <div class="cont-elements">
               <h3>Intervalo de datas</h3>
               <br>
-              <p>
-                O calendário permite aos usuários escolher uma data ou um
-                período de visualização de informações.
-              </p>
-              <br>
-              <el-date-picker v-model="value1" type="daterange" range-separator="-" start-placeholder="Início"
-                end-placeholder="Término" :prefix-icon="customPrefix" :size="size" />
+
+              <el-row>
+                <el-col :span="12">
+                  <h6>Default</h6>
+                  <br>
+                  <el-date-picker v-model="value1" type="daterange" range-separator="-" start-placeholder="Início"
+                    end-placeholder="Término" :prefix-icon="customPrefix" :size="size" />
+                </el-col>
+                <el-col :span="12">
+                  <h6>Disabled</h6>
+                  <br>
+                  <el-date-picker v-model="value1" type="daterange" range-separator="-" disabled
+                    start-placeholder="Início" end-placeholder="Término" :prefix-icon="customPrefix" :size="size" />
+                </el-col>
+              </el-row>
 
               <br>
               <br>
               <el-collapse v-model="activeNames" @change="handleChange">
                 <el-collapse-item title="Ver código">
                   <div class="html_codigos">
-                    &#60;<span tagColor>template</span> &#62;
+                    &#60;<span tagColor>template</span>&#62;
                     <div class="border-left">
                       <div recuo>
-                        <span tagColor>&#60;el-date-picker</span>
+                        &#60;<span tagColor>el-date-picker</span>
                         <div class="border-left">
                           <div recuo>
                             v-model=<span roxo>"value1"</span> <br />type=<span roxo>"daterange"</span><br />
@@ -124,7 +156,24 @@
                             :size=<span roxo>"size"</span>
                           </div>
                         </div>
-                        <span tagColor>/&#62;</span>
+                        /&#62;
+                      </div>
+                    </div>
+                    <div class="border-left">
+                      <div recuo>
+                        &#60;<span tagColor>el-date-picker</span>
+                        <div class="border-left">
+                          <div recuo>
+                            v-model=<span roxo>"value1"</span> <br />type=<span roxo>"daterange"</span><br />
+                            range-separator=<span roxo>"-"</span> <br />start-placeholder=<span roxo>"Início"</span>
+                            <br>
+                            end-placeholder=<span roxo>"Término"</span><br />:prefix-icon=<span
+                              roxo>"customPrefix"</span><br />
+                            :size=<span roxo>"size"</span><br>
+                            disabled
+                          </div>
+                        </div>
+                        /&#62;
                       </div>
                     </div>
                     &#60;/<span tagColor>template</span>&#62;
@@ -163,12 +212,9 @@
                       </div>
                     </div>
                     };
-
                     <br>
                     &#60;/<span tagColor>script</span>&#62;
                   </div>
-
-
                 </el-collapse-item>
               </el-collapse>
             </div>

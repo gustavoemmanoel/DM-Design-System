93fccbb8c8deb1b024ccef9359dba579b8f3d5d5
MateusDuraessantos <mateusduraessantos@gmail.com>
2023-03-14 13:39:09 -0300

    Otimização cards

--- src/views/Componentes/CardsDs.vue
+++ src/views/Componentes/CardsDs.vue
@@ -5,278 +5,154 @@
     </el-header>
     <el-container>
       <el-main>
+
+
         <div class="containerInterno">
           <div>
             <h1>/Cards</h1>
             <hr />
-          </div>
-          <br />
-          <div class="cont-elements">
             <p>
               Cards permitem a visualização das principais informações de modo
               sintético e rápido. Eles podem ser estáticos ou clicáveis,
               revelando mais detalhes após interação.
             </p>
-            <br>
-            <div class="container-grid">
-              <!-- Cards clicáveis -->
-              <span>
-                <h4>Cards clicáveis</h4>
-                <br>
-                <dm-card class="dm-card--clicavel" tabindex="0">
-                  <h4>Mercantil</h4>
-                  <p class="body_02">R$ 555,03</p>
-                </dm-card>
-              </span>
-              <!-- Cards estático -->
-              <span>
-                <h4>Cards estático</h4>
-                <br>
-                <dm-card class="dm-card--estatico">
-                  <h4>Renda média</h4>
-                  <p class="body_02">R$ 555,03</p>
-                </dm-card>
-              </span>
-              <!-- Card Romaneio -->
-              <span>
-                <h4>Card Romaneio</h4>
-                <br>
-                <dm-card class="dm-card--romaneio">
-                  <dm-card-bar class="dm_rosa_00" />
-                  <dm-card-content tabindex="0">
-                    <h4>Romaneio 01</h4>
-                    <div>Ocupação: 40%</div>
-                    <div>Peso: 1294 kg</div>
-                    <div>Volumes: 40%</div>
-                  </dm-card-content>
-                </dm-card>
-              </span>
-            </div>
-
-            <br />
-
-            <el-collapse v-model="activeNames" @change="handleChange">
-              <el-collapse-item title="Ver código">
-                <span class="html_codigos">
-                  <span green>&#60;!-- card clicável --&#62;</span>
-                  <br />
-                  &#60;<span tagColor>dm-card</span> class=<span roxo>"dm-card--clicavel"</span> tabindex=<span
-                    roxo>"0"</span>&#62;
-                  <div class="border-left">
-                    <div recuo>
-                      &#60;<span tagColor>h4</span>&#62;<span black>Mercantil</span>&#60;/<span tagColor>h4</span>&#62;
-                      <br>
-                      &#60;<span tagColor>p</span>&#62;<span black>R$ 555,03</span>&#60;/<span tagColor>p</span>&#62;
-
-                      <br />
-                    </div>
-                  </div>
-                  &#60;/<span tagColor>dm-card</span>&#62;
-                </span>
-                <br />
-
-                <br />
-                <span class="html_codigos">
-                  <span green>&#60;!-- card estático --&#62;</span>
-                  <br />
-                  &#60;<span tagColor>dm-card</span> class=<span roxo>"dm-card--estatico"</span>&#62;
-                  <div class="border-left">
-                    <div recuo>
-                      &#60;<span tagColor>h4</span>&#62;<span black>Renda média</span>&#60;/<span tagColor>h4</span>&#62;
-                      <br>
-                      &#60;<span tagColor>p</span>&#62;<span black>R$ 555,03</span>&#60;/<span tagColor>p</span>&#62;
-                      <br />
-                    </div>
-                  </div>
-                  &#60;/<span tagColor>dm-card</span>&#62;
-                </span>
-                <br />
-
-                <br />
-                <span class="html_codigos">
-                  <span green>&#60;!-- card romaneio --&#62;</span>
-                  <br />
-                  &#60;<span tagColor>dm-card</span> class=<span roxo>"dm-card--romaneio"</span>&#62;
-                  <div class="border-left">
-                    <div recuo>
-                      &#60;<span tagColor>dm-card-bar</span> class=<span roxo>"dm_rosa_00"</span>/&#62;<br />
-                      &#60;<span tagColor>dm-card-content</span> tabindex=<span roxo>"0"</span>&#62;
-                      <div class="border-left">
-                        <div recuo>
-                          &#60;<span tagColor>h4</span>&#62;<span black>Romaneio 01</span>&#60;/<span
-                            tagColor>h4</span>&#62;
-                          <br />
-                          &#60;<span tagColor>div</span>&#62;<span black>Ocupação: 40%</span>&#60;/<span
-                            tagColor>div</span>&#62;
-                          <br />
-                          &#60;<span tagColor>div</span>&#62;<span black>Peso: 1294 kg</span>&#60;/<span
-                            tagColor>div</span>&#62;
-                          <br />
-                          &#60;<span tagColor>div</span>&#62;<span black>Volumes: 40%</span>&#60;/<span
-                            tagColor>div</span>&#62;
-                        </div>
-                      </div>
-                      &#60;/<span tagColor>dm-card-content</span>&#62;
-                    </div>
-                  </div>
-                  &#60;/<span tagColor>dm-card</span>&#62;
-                </span>
-
-              </el-collapse-item>
-            </el-collapse>
           </div>
+          <br>
 
           <div class="cont-elements">
-            <h3>Cards clicáveis</h3>
+            <h3>Card clicáveis</h3>
             <br>
             <p>Cards clicáveis possuem comportamento de botão. Além de apresentar as principais informações resumidas, ao
               serem clicados, podem expandir ou apresentar uma nova informação na página.</p>
             <br>
+            <div class="container-grid">
+              <dm-card @click="cardClicked">
+                <h4>Mercantil</h4>
+                <p class="body_02">R$ 555,03</p>
+              </dm-card>
+              <dm-card @click="cardClicked">
+                <h4>Mercantil</h4>
+                <p class="body_02">R$ 555,03</p>
+              </dm-card>
+              <dm-card @click="cardClicked">
+                <h4>Mercantil</h4>
+                <p class="body_02">R$ 555,03</p>
+              </dm-card>
+            </div>
+            <br>
+            <br>
             <img style="width: 93%;" src="cards/01.svg">
+
             <br>
+            <br>
+            <p class="detail_02">Cards azuis com função de botão: ao clicar em um deles, o gráfico muda para mostrar as
+              informações
+              desejadas. Os cards do tipo botão usam o comportamento de cores do botão. Ativos ficam azuis e clicados
+              ficam amarelos. A sombra também é uma detalhe que identifica o botão.</p>
+
             <br>
             <el-collapse v-model="activeNames" @change="handleChange">
               <el-collapse-item title="Ver código">
-                <span class="html_codigos">
-                  &#60;<span tagColor>dm-card</span> class=<span roxo>"dm-card--clicavel"</span> tabindex=<span
-                    roxo>"0"</span>&#62;
-                  <div class="border-left">
-                    <div recuo>
-                      &#60;<span tagColor>h4</span>&#62;<span black>Mercantil</span>&#60;/<span tagColor>h4</span>&#62;
-                      <br>
-                      &#60;<span tagColor>p</span>&#62;<span black>R$ 555,03</span>&#60;/<span tagColor>p</span>&#62;
 
-                      <br />
-                    </div>
-                  </div>
-                  &#60;/<span tagColor>dm-card</span>&#62;
-                </span>
-              </el-collapse-item>
-            </el-collapse>
-
-          </div>
-
-          <div class="cont-elements">
-            <h3>Cards estáticos</h3>
-            <br>
-            <p>Cards estáticos possuem como única funcionalidade apresentar as informações desejadas, não causando
-              alterações no restante das telas.</p>
-            <br>
-
-
-            <div style="display: grid; grid-template-columns: 250px 250px; gap: 30px; width: max-content; margin: auto;">
-              <dm-card class="dm-card--estatico">
-                <h4>Renda média</h4>
-                <p class="body_02">R$ 555,03</p>
-              </dm-card>
-              <dm-card class="dm-card--estatico">
-                <h4>Renda média</h4>
-                <p class="body_02">R$ 555,03</p>
-              </dm-card>
-              <dm-card class="dm-card--estatico">
-                <h4>Renda média</h4>
-                <p class="body_02">R$ 555,03</p>
-              </dm-card>
-              <dm-card class="dm-card--estatico">
-                <h4>Renda média</h4>
-                <p class="body_02">R$ 555,03</p>
-              </dm-card>
-            </div>
-            <br>
-            <p class="body_03">Cards estáticos apenas para visualização de informações pontuais.</p>
-            <br>
-            <el-collapse v-model="activeNames" @change="handleChange">
-              <el-collapse-item title="Ver código">
-                <span class="html_codigos">
-                  &#60;<span tagColor>dm-card</span> class=<span roxo>"dm-card--estatico"</span>&#62;
-                  <div class="border-left">
-                    <div recuo>
-                      &#60;<span tagColor>h4</span>&#62;<span black>Renda média</span>&#60;/<span tagColor>h4</span>&#62;
-                      <br>
-                      &#60;<span tagColor>p</span>&#62;<span black>R$ 555,03</span>&#60;/<span tagColor>p</span>&#62;
-                      <br />
-                    </div>
-                  </div>
-                  &#60;/<span tagColor>dm-card</span>&#62;
-                </span>
-              </el-collapse-item>
-            </el-collapse>
-
-          </div>
-
-          <div class="cont-elements">
-            <h3>Cards de romaneio</h3>
-            <br>
-            <p>Cards de romaneios tem características especiais. Além de mostrar uma lista de informações, ao contrário
-              dos estáticos que só informam uma, também podem ser clicáveis para exibir uma lista de viagens.</p>
-            <br>
-
-            <div style="display: flex; justify-content: center; width: 100%; gap: 40px;">
-              <dm-card class="dm-card--romaneio">
-                <dm-card-bar class="dm_rosa_00" />
-                <dm-card-content tabindex="0">
-                  <h4>Romaneio 01</h4>
-                  <div>Ocupação: 40%</div>
-                  <div>Peso: 1294 kg</div>
-                  <div>Volumes: 40%</div>
-                </dm-card-content>
-              </dm-card>
-
-              <dm-card class="dm-card--romaneio">
-                <dm-card-bar class="dm_azul_00" />
-                <dm-card-content tabindex="0">
-                  <h4>Romaneio 01</h4>
-                  <div>Ocupação: 40%</div>
-                  <div>Peso: 1294 kg</div>
-                  <div>Volumes: 40%</div>
-                </dm-card-content>
-              </dm-card>
-              <dm-card class="dm-card--romaneio">
-                <dm-card-bar class="dm_laranja_00" />
-                <dm-card-content tabindex="0">
-                  <h4>Romaneio 01</h4>
-                  <div>Ocupação: 40%</div>
-                  <div>Peso: 1294 kg</div>
-                  <div>Volumes: 40%</div>
-                </dm-card-content>
-              </dm-card>
-            </div>
-            <br>
-            <p>Cards de romaneio para visualização de informações pontuais e com função de botão: ao clicar nele, abre uma
-              aba com todas as informações do romaneio e seus pedidos, podendo ser visualizada e editada.</p>
-            <br>
-            <el-collapse v-model="activeNames" @change="handleChange">
-              <el-collapse-item title="Ver código">
-                <span class="html_codigos">
-                  &#60;<span tagColor>dm-card</span> class=<span roxo>"dm-card--romaneio"</span>&#62;
-                  <div class="border-left">
-                    <div recuo>
-                      &#60;<span tagColor>dm-card-bar</span> class=<span roxo>"dm_rosa_00"</span>/&#62;<br />
-                      &#60;<span tagColor>dm-card-content</span> tabindex=<span roxo>"0"</span>&#62;
-                      <div class="border-left">
-                        <div recuo>
-                          &#60;<span tagColor>h4</span>&#62;<span black>Romaneio 01</span>&#60;/<span
-                            tagColor>h4</span>&#62;
-                          <br />
-                          &#60;<span tagColor>div</span>&#62;<span black>Ocupação: 40%</span>&#60;/<span
-                            tagColor>div</span>&#62;
-                          <br />
-                          &#60;<span tagColor>div</span>&#62;<span black>Peso: 1294 kg</span>&#60;/<span
-                            tagColor>div</span>&#62;
-                          <br />
-                          &#60;<span tagColor>div</span>&#62;<span black>Volumes: 40%</span>&#60;/<span
-                            tagColor>div</span>&#62;
-                        </div>
-                      </div>
-                      &#60;/<span tagColor>dm-card-content</span>&#62;
-                    </div>
-                  </div>
-                  &#60;/<span tagColor>dm-card</span>&#62;
-                </span>
               </el-collapse-item>
             </el-collapse>
           </div>
+
+          <br />
+
         </div>
+
+
+
+        <div class="cont-elements">
+          <h3>Card estático</h3>
+          <br>
+          <p>Cards estáticos possuem como única funcionalidade apresentar as informações desejadas, não causando
+            alterações no restante das telas.</p>
+          <br>
+          <div class="container-grid">
+            <!-- Cards clicáveis -->
+
+            <dm-card-estatico>
+              <h4>Renda média</h4>
+              <p class="body_02">R$ 555,03</p>
+            </dm-card-estatico>
+
+            <dm-card-estatico>
+              <h4>Renda média</h4>
+              <p class="body_02">R$ 555,03</p>
+            </dm-card-estatico>
+
+            <dm-card-estatico>
+              <h4>Renda média</h4>
+              <p class="body_02">R$ 555,03</p>
+            </dm-card-estatico>
+
+          </div>
+
+          <br>
+          <p class="detail_02">Cards estáticos apenas para visualização de informações pontuais.</p>
+          <br>
+          <el-collapse v-model="activeNames" @change="handleChange">
+            <el-collapse-item title="Ver código">
+
+            </el-collapse-item>
+          </el-collapse>
+        </div>
+        <div class="cont-elements">
+          <h3>Cards de romaneio</h3>
+          <br>
+          <p>Cards de romaneios tem características especiais. Além de mostrar uma lista de informações, ao contrário
+            dos estáticos que só informam uma, também podem ser clicáveis para exibir uma lista de viagens.</p>
+          <br>
+          <div class="container-grid">
+            <dm-card-romaneio @click="cardRomaneio">
+              <dm-card-bar class="dm_laranja_01" />
+              <dm-card-content>
+                <h4>Romaneio 01</h4>
+                <div>Ocupação: 40%</div>
+                <div>Peso: 1294 kg</div>
+                <div>Volumes: 40%</div>
+              </dm-card-content>
+            </dm-card-romaneio>
+
+            <dm-card-romaneio @click="cardRomaneio">
+              <dm-card-bar class="dm_verde_00" />
+              <dm-card-content>
+                <h4>Romaneio 01</h4>
+                <div>Ocupação: 40%</div>
+                <div>Peso: 1294 kg</div>
+                <div>Volumes: 40%</div>
+              </dm-card-content>
+            </dm-card-romaneio>
+
+            <dm-card-romaneio @click="cardRomaneio">
+              <dm-card-bar class="dm_rosa_00" />
+              <dm-card-content>
+                <h4>Romaneio 01</h4>
+                <div>Ocupação: 40%</div>
+                <div>Peso: 1294 kg</div>
+                <div>Volumes: 40%</div>
+              </dm-card-content>
+            </dm-card-romaneio>
+
+
+          </div>
+          <br>
+          <p class="detail_02">Cards de romaneio para visualização de informações pontuais e com função de botão: ao clicar nele, abre uma
+            aba com todas as informações do romaneio e seus pedidos, podendo ser visualizada e editada.</p>
+          <br>
+          <el-collapse v-model="activeNames" @change="handleChange">
+            <el-collapse-item title="Ver código">
+
+            </el-collapse-item>
+          </el-collapse>
+
+        </div>
+
+
+
+
       </el-main>
     </el-container>
   </el-container>
@@ -294,6 +170,68 @@ export default {
       headerTitle: "Cards",
     };
   },
+
+  mounted() {
+    this.indexadorCard()
+    this.indexadorRomaneio()
+  },
+
+  methods: {
+    cardRomaneio(event) {
+      const selectedRomaneio = event.currentTarget
+
+      const romaneioAtivo = document.querySelector('[romaneio-ativo]')
+
+      if (romaneioAtivo == null) {
+        selectedRomaneio.setAttribute('romaneio-ativo', '')
+
+        document.querySelector('[romaneio-ativo]').childNodes[1].style.borderColor = `var(--${document.querySelector('[romaneio-ativo]').childNodes[0].classList})`
+      }
+      else {
+        romaneioAtivo.childNodes[1].removeAttribute('style')
+        romaneioAtivo.removeAttribute('romaneio-ativo')
+        selectedRomaneio.childNodes[1].style.borderColor = `var(--${selectedRomaneio.childNodes[0].classList})`
+        selectedRomaneio.setAttribute('romaneio-ativo', '')
+      }
+    },
+
+    indexadorRomaneio() {
+      const cardsRomaneio = document.querySelectorAll('dm-card-romaneio')
+
+      for (let i = 0; i < cardsRomaneio.length; i++) {
+        cardsRomaneio[i].setAttribute('id', `romaneio-${i}`)
+      }
+    },
+
+    indexadorCard() {
+      const card = document.querySelectorAll('dm-card')
+      for (let i = 0; i < card.length; i++) {
+        card[i].setAttribute('id', `card-${i}`)
+      }
+    },
+
+    cardClicked(event) {
+      const ativadoCard = document.querySelector('[card-ativo]')
+
+      if (ativadoCard == null) {
+        cardDm()
+      } else {
+        ativadoCard.removeAttribute('card-ativo')
+        cardDm()
+      }
+
+      function cardDm() {
+        const card1 = event.target.parentNode.getAttribute('id')
+        const card2 = event.target.getAttribute('id')
+        if (card1 == null) {
+          document.getElementById(card2).setAttribute('card-ativo', '')
+
+        } else {
+          document.getElementById(card1).setAttribute('card-ativo', '')
+        }
+      }
+    },
+  }
 };
 </script>
 
--- src/views/Componentes/UploadDs.vue
+++ src/views/Componentes/UploadDs.vue
@@ -18,11 +18,11 @@
                 action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15" multiple>
                 <div class="el-upload__text">
                   <dm-icon upload />
-                  Drop file here or <em>click to upload</em>
+                  Arraste um arquivo aqui ou <em>Clique para fazer upload</em>
                 </div>
                 <template #tip>
                   <div class="el-upload__tip">
-                    jpg/png files with a size less than 500kb
+                    Arquivos jpg/png inferiores a 500kb
                   </div>
                 </template>
               </el-upload>
